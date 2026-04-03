84b56976745de873c91ce8f9bff1e1f3c3f6a12d
Mayur <mayurk.yadav2023@gmail.com>
2026-04-03 07:18:56 -0400

    The Verify/  endpoint is added

---
+++ MayurkumarApi/Controllers/VerifyController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MayurkumarApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class VerifyController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            // get runner name from docker env variable
+            var runner = Environment.GetEnvironmentVariable("RUN_BY") ?? "RUN_BY not set";
+            // create response object for /verify
+            var response = new
+            {
+                builder = "Mayurkumar",
+                runner = runner,
+                timestamp = DateTime.UtcNow,
+                machineName = Environment.MachineName
+            };
+
+            return Ok(response);
+        }
+    }
+}
